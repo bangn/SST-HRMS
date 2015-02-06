2c13e08476930011a3c386673ac957bc1fc08692
Bang Nguyen <bang@getup.org.au>
2015-02-06 23:55:14 +1100

    Add dashboard for normal user; add tab to admin user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get '/dashboard' => 'dashboard#index'
 
   namespace :admin do
     root :to => "dashboards#index"
