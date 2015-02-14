b57a9fbb7d1fa8566988e6d6de85c0620c8d77ed
Bang Nguyen <bang@getup.org.au>
2015-02-14 20:45:09 +1100

    descriptive name, avoid confusion with params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,14 +5,14 @@ class Admin::UsersController < Admin::AdminsController
 
   def search
     @users = User.where("email = ? or first_name = ? or last_name = ?", params[:email], params[:first_name], params[:last_name])
-    @users = User.all if @users.empty? && params_empty?
+    @users = User.all if @users.empty? && search_params_empty?
 
     @users = @users.page params[:page]
     render :index
   end
 
   private
-  def params_empty?
+  def search_params_empty?
     [:email, :first_name, :last_name].inject(true) {|result, search_param| result && params[search_param].empty? }
   end
 end
