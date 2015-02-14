6db932053f6672f532b988d9380b45f99a728df3
Bang Nguyen <bang@getup.org.au>
2015-02-14 20:57:49 +1100

    clean up

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Admin::UsersController do
-
+  describe "search user" do
+    it "return all users if params is empty" ,focus: true do
+      byebug
+    end
+  end
 end
