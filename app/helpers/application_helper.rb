5a1a2bfba3e62c74325c639be741aede0fbee02e
Bang Nguyen <bang@getup.org.au>
2015-02-15 13:28:48 +1100

    Do not show empty message; do not show sign out message as page is redirected to home after signing out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def show_error_messages_if_any messages
-    return "" if !messages or messages.empty?
+    return "" if !messages or !messages.any?
 
     messages = messages.map { |msg| content_tag(:li, msg) }.join
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   paginates_per 10
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :password_confirmation, presence: true, length: {minimum: 8}
 
   def full_name
