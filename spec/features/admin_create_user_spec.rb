40a5c9b04978e2daae33bf52b42f79de81c878fa
Bang Nguyen <bang@getup.org.au>
2015-02-15 18:34:32 +1100

    cannot create new user without email

--- spec/features/admin_create_user_spec.rb
+++ spec/features/admin_create_user_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe "Admin create user" do
   let!(:admin) { FactoryGirl.create(:admin) }
-  it "should create new user" do
+  
+  before :each do
     sign_in_as!(admin)
     click_link "Create new user"
-
+  end
+  
+  it "should create new user" do
     fill_in "Email", with: "new_user@example.com"
     fill_in "First name", with: "First"
     fill_in "Last name", with: "Last"
@@ -15,5 +18,13 @@ describe "Admin create user" do
     click_button "Create"
     
     expect(page).to have_content("new_user@example.com")
+  end
+
+  it "should not create user with out email" do
+    fill_in "Email", with: ""
+    click_button "Create"
+
+    byebug
+    expect(page).to have_content "Email can't be blank"
   end
 end
