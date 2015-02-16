6b111ce41c56962a740750e0f2eb095f32f08120
Bang Nguyen <bang@getup.org.au>
2015-02-17 00:41:46 +1100

    Revert "yet another try"
    This reverts commit f1ab6076b68bf543e487929eaa0babfe182f45c0.
    
    Conflicts:
    	config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
-  config.assets.paths << "#{Rails.root}/app/assets/fonts"
-  config.font_assets.origin = 'http://codeschool.com'
 end
