f1ab6076b68bf543e487929eaa0babfe182f45c0
Bang Nguyen <bang@getup.org.au>
2015-02-17 00:23:44 +1100

    yet another try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.paths << "#{Rails.root}/app/assets/fonts"
 end
