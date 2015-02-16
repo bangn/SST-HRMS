2d7255578800c2a8edf798432795a8588ed6771e
Bang Nguyen <bang@getup.org.au>
2015-02-17 00:40:17 +1100

    Revert "another try, font awesome does not display on heroku"
    This reverts commit fc0671058699ab65f2735fa8a6f27c9c261197e7.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module SSTHrms
   class Application < Rails::Application
-    Bundler.require(:default, :assets, Rails.env)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
