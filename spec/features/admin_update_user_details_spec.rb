65fcb27213d3bb8b36defa58c32bac7e6ccf59ba
Bang Nguyen <bang@getup.org.au>
2015-02-18 23:43:29 +1100

    scenario test

--- spec/features/admin_update_user_details_spec.rb
+++ spec/features/admin_update_user_details_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe "Admin update user detail" do
+describe "Admin update details" do
   let!(:admin) { FactoryGirl.create(:admin) }
   let!(:user) { FactoryGirl.create(:user) }
   
   before :each do
     sign_in_as!(admin)
-    click_link user.email
   end
   
-  it "should update details without providing new password" do
+  it "should be able update user details without providing new password" do
+    click_link user.email
     fill_in "First name", with: "First"
     fill_in "Last name", with: "Last"
 
@@ -19,6 +19,12 @@ describe "Admin update user detail" do
   end
 
   it "should disable email field" do
+    click_link user.email
     page.find("#user_email").should be_disabled
+  end
+  
+  it "should not be able to update its own role" do
+    click_link admin.email
+    page.find("#user_admin").should be_disabled
   end
 end
