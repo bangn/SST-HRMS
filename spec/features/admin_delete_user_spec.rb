3c9a0f0b0201e96f78fcc284dc293a7033b79413
Bang Nguyen <bang@getup.org.au>
2015-02-21 03:21:09 +1100

    render modal properly

--- spec/features/admin_delete_user_spec.rb
+++ spec/features/admin_delete_user_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe "Admin delete user" do
   let!(:admin) { FactoryGirl.create(:admin) }
   let!(:user) { FactoryGirl.create(:user) }
+  let!(:user_2) { FactoryGirl.create(:user) }
   
   before :each do
     admin.confirm!
     user.confirm!
+    user_2.confirm!
     sign_in_as!(admin)
   end
   
@@ -15,6 +17,14 @@ describe "Admin delete user" do
     page.find("#delete-confirm").click
 
     expect(page).not_to have_content("#{user.email}")
+    expect(page).to have_content("#{user_2.email}")
+  end
+  
+  it "should not delete its own", :js => true, :focus => true do
+    page.find("#user-#{admin.id}").click
+    page.find("#delete-confirm").click
+    
+    expect(page).to have_content("You can't delete yourself!")
   end
   
   it "should not have delete link for current user", :js => true do
