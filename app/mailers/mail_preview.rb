b37570701dfec4d5a703540c9a823c339514bf2d
Bang Nguyen <bang@getup.org.au>
2015-02-21 00:03:47 +1100

    add mail preview for confirmation, reset, unlock

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,6 +1,19 @@
 class MailPreview < MailView
+  @@user = User.new :email => "user@user.com", :password => "password", :password_confirmation => "password"
+
   def welcome_to_strongtie
-    user = User.new :email => "user@user.com", :password => "password", :password_confirmation => "password"
-    UserMailer.welcome_to_strongtie user
+    UserMailer.welcome_to_strongtie @@user
+  end
+  
+  def confirmation_instructions
+    Devise::Mailer.confirmation_instructions(@@user, "faketoken")
+  end
+
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(@@user, "faketoken")
+  end
+  
+  def unlock_instructions
+    Devise::Mailer.unlock_instructions(@@user, "faketoken")
   end
 end
