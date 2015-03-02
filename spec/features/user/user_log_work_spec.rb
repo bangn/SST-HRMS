08efc0c610c7316b4376946e22c006dcadc56bad
Bang Nguyen <bang@getup.org.au>
2015-03-02 23:03:18 +1100

    Add test if working date is blank, duration is less than or equal 0

--- spec/features/user/user_log_work_spec.rb
+++ spec/features/user/user_log_work_spec.rb
@@ -21,4 +21,29 @@ describe 'User log work' do
     expect(page).to have_content("New work log has been created successfully!")
     expect(page).to have_content("This is a test work log")
   end
+  
+  it "should show error if date is blank" do
+    click_link "TIMESHEETS"
+    click_link "Log work"
+    
+    fill_in "Description", with: "This is a test work log"
+    fill_in "Duration", with: 2
+    
+    click_button "SAVE"
+    
+    expect(page).to have_content("Working date can't be blank")
+  end
+  
+  it "should show error if duration is zero" do
+    click_link "TIMESHEETS"
+    click_link "Log work"
+    
+    fill_in "Description", with: "This is a test work log"
+    fill_in "Duration", with: 0
+    fill_in "Working date", with: "02-03-2015"
+    
+    click_button "SAVE"
+    
+    expect(page).to have_content("Duration must be greater than 0")
+  end
 end
