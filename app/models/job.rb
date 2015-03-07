5ae8c60448f8341aeca6a94255df82e26bb9a37e
Bang Nguyen <bang@getup.org.au>
2015-03-07 22:03:28 +1100

    job should be completed if end date present

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,6 +9,7 @@ class Job < ActiveRecord::Base
   validates :start_date, :presence => {:message => "should be presented!"}, :if => "end_date.present?"
   validates :end_date, :presence => {:message => "should be presented!"}, :if => "state == State.COMPLETED.first"
   validate :validate_job_time_range
+  validate :validate_job_status, :if => "end_date.present?"
   
   def description
     team_name = team.nil? ? "Unknown" : team.name
@@ -18,6 +19,12 @@ class Job < ActiveRecord::Base
   def validate_job_time_range
     if start_date && end_date && (start_date > end_date)
       errors.add(:end_date, 'should be on the same day as start date or in the future')
+    end
+  end
+  
+  def validate_job_status
+    if state != State.COMPLETED.first
+      errors.add(:state, 'should be completed!')
     end
   end
 end
