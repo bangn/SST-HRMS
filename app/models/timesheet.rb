7c9f9c3f8f687112663edc9ee8b8818a287d9906
Bang Nguyen <bang@getup.org.au>
2015-03-22 03:33:28 +1100

    Allow description to be editable in table view; do not squish as it will remove breakline in descrioption

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -2,7 +2,7 @@ class Timesheet < ActiveRecord::Base
   paginates_per 10
   belongs_to :user
   belongs_to :job
-  auto_strip_attributes :description, :duration, :squish => true
+  auto_strip_attributes :description, :duration
   validates :description, length: {minimum: 5}
   validates :duration, presence: true, length: {minimum: 1}, numericality: {greater_than: 0}
   validates :working_date, presence: true
