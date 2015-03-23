bd709d67ed313ded458ff5cc1c5ef4e31ebd1282
Bang Nguyen <bang@getup.org.au>
2015-03-23 22:15:21 +1100

    nicer date format

--- app/assets/javascripts/x-editable.js
+++ app/assets/javascripts/x-editable.js
@@ -1,3 +1,10 @@
 function enableXEditable() {
-  $('.editable').editable();
+  $('.editable').editable({
+    format: 'DD-MM-YYYY',    
+    template: 'DD / MMM / YYYY',    
+    combodate: {
+      minYear: 2000,
+      maxYear: 2025
+    }
+  });
 }  
